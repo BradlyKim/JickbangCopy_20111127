55518b43f812f5d4f057925bf3dc7c026df2905a
BradlyKim <kjh5864@naver.com>
2021-11-27 14:35:59 +0900

    메인화면 주석 작업중

--- app/src/main/java/com/nepplus/jickbangcopy_20111127/MainActivity.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20111127/MainActivity.kt
@@ -9,9 +9,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+//    방 목록을 담기 위한 그릇
+    val mRoomList = ArrayList<RoomData>()
+
+//    변수는 멤버변수로 -> 대입은 onCreate{} (나중에) : lateinit var
     lateinit var mRoomAdapter: RoomAdapter
 
-    val mRoomList = ArrayList<RoomData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
